8332db4c66a12b30f3d2422991ec02fa9efd0b69
PurplikDev <vojta.g@atlas.cz>
2024-07-14 02:32:08 +0200

    camera adjustments

--- scripts/component/anchored_camera.gd
+++ scripts/component/anchored_camera.gd
@@ -14,8 +14,8 @@ func _process(delta):
 	zoom = lerp(zoom, Vector2(current_zoom, current_zoom), lerp_speed * delta)
 
 func zoom_in():
-	current_zoom = clampf(current_zoom + zoom_amount, 2.5, 5.0)
+	current_zoom = clampf(current_zoom + zoom_amount, 3.0, 5.0)
 	
 
 func zoom_out():
-		current_zoom = clampf(current_zoom - zoom_amount, 2.5, 5.0)
+		current_zoom = clampf(current_zoom - zoom_amount, 3.0, 5.0)
